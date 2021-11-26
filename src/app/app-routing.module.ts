f9841e03c907194e49cadebb75043f8f95257913
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-11-26 21:43:40 +0300

    Began the routing process and styled the navbar some more.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'landing-page', component: LandingPageComponent},
+  {path: 'search-form', component: SearchFormComponent},
+  {path: '', redirectTo: 'landing-page', pathMatch: 'full'},
+  {path: '**', component: }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
