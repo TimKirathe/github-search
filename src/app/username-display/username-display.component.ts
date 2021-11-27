70acd2edf12fbe484a8493149d3a7c3cbd384c99
Timothy Kirathe <timothykirathe@Timothys-MacBook-Pro.local>
2021-11-27 11:13:56 +0300

    Added an Input decorator used to transfer data from the form to the username-display component

--- src/app/username-display/username-display.component.ts
+++ src/app/username-display/username-display.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-username-display',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./username-display.component.css']
 })
 export class UsernameDisplayComponent implements OnInit {
+
+  @Input() users: User[] = [];
 
   constructor() { }
 
